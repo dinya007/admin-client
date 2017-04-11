6b30cea7765df08652c5323a9abcf93f7c52e4b8
denis <tlasikee93>
2017-04-11 16:13:04 +0300

    форма логина

--- app/app.js
+++ app/app.js
@@ -1,16 +1,37 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', [
+    'ngRoute',
+    'ngCookies'
+]);
 
-app.config(function ($routeProvider) {
-    $routeProvider.when('/', {
-        controller: 'loginController',
-        templateUrl: 'app/view/login.html'
-    })
+app.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
         .when('/login', {
-            controller: 'loginController',
-            templateUrl: 'app/view/login.html'
+            controller: 'LoginController',
+            templateUrl: 'app/view/login.html',
+            hideMenus: true
         })
-        .otherwise({
-            redirectTo: '/'
-        });
-});
 
+        .when('/', {
+            controller: 'HomeController',
+            templateUrl: 'app/view/home.html'
+        })
+
+        .otherwise({redirectTo: '/login'});
+}]);
+
+app.run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+                $location.path('/login');
+            }
+        });
+    }]);
